5b1f79c9f5a07da610cac37065be62cf465dc8df
Armughan Ahmed <armughan.ahmed@yahoo.com>
2020-08-08 13:04:26 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+// IMPORT REACT AND REACTDOM LIBRARIES
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
+
+// CREATE REACT COMPONENT
 const App=()=>{
     return <div>Hello there</div>
 };
 
+
+
+// TAKE THE REACT COMPONENT AND SHOW IT ON THE SCREEN
 ReactDOM.render(
     <App/>,
     document.querySelector('#root')
